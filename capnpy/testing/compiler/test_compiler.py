9486d63cf2a4233a89dd9b720a6d09578203a4bc
Ben Moran <ben@gambitresearch.com>
2017-03-01 11:39:57 +0000

    Failing compilation test for Text constants

--- capnpy/testing/compiler/test_compiler.py
+++ capnpy/testing/compiler/test_compiler.py
@@ -194,19 +194,22 @@ class TestCompilerOptions(CompilerTest):
         @0xbf5147cbbecf40c1;
         struct Foo {
             const bar :UInt16 = 42;
+            const baz :Text = "baz";
         }
         """
         mod = self.compile(schema)
         assert mod.Foo.bar == 42
+        assert mod.Foo.baz == "baz"
 
     def test_global_const(self):
         schema = """
         @0xbf5147cbbecf40c1;
         const bar :UInt16 = 42;
+        const baz :Text = "baz";
         """
         mod = self.compile(schema)
         assert mod.bar == 42
-
+        assert mod.baz == "baz"
 
 class TestCapnpExcecutable(CompilerTest):
 
